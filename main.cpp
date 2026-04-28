36af1066a46b3c6f33e1bfac983853d3b67dcdab
Angel Mora <angelmora216@gmail.com>
2026-04-28 13:08:22 -0700

    Implementation of the partition function

--- main.cpp
+++ main.cpp
@@ -10,3 +10,56 @@
 #include <vector>
 
 using namespace std;
+
+// The partition function
+int Partition(vector<int>& numbers, int i, int k) {
+    int midpoint = i + (k - i) / 2;
+    int pivot = numbers.at(midpoint);
+
+    int low = i;
+    int high = k;
+
+    bool done = false;
+    while (!done) {
+        while (numbers.at(low) < pivot) {
+            low++;
+        }
+
+        while (numbers.at(high) > pivot) {
+            high--;
+        }
+
+        if (low >= high) {
+            done = true;
+        } else {
+            int temp = numbers.at(low);
+            numbers.at(low) = numbers.at(high);
+            numbers.at(high) = temp;
+
+            low++;
+            high--;
+        }
+    }
+
+    return high;
+}
+
+int main() {
+    vector<int> numbers = {10, 2, 78, 4, 45, 32, 7, 11};
+
+    cout << "Original: ";
+    for (int n : numbers) cout << n << " ";
+    cout << endl;
+
+    int lastLeftIndex = Partition(numbers, 0, numbers.size() - 1);
+
+    cout << "After Partition: ";
+    for (int n : numbers) cout << n << " ";
+    cout << endl;
+
+    cout << "Partition Index: " << lastLeftIndex << endl;
+    cout << "Left side ends at index " << lastLeftIndex << " (value: "
+    << numbers[lastLeftIndex] << ")" << endl;
+
+    return 0;
+}
