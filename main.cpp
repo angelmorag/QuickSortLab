28e430309939543ef431b6585204eabe84ee79b1
Angel Mora <angelmora216@gmail.com>
2026-04-28 12:51:26 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/* Name: Angel Mora Gutierrez
+ * Instructor: Professor Vaughn
+ * Course:MCSCI-272
+ * Lab: Quick Sort Lab
+ * Purpose: Implementation of the Quick Sort algorithm using the midpoint
+ * partition strategy
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
